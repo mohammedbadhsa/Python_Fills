479bf094f8ffc1869da00d22ebd391a08e570b14
mohammed-badsha <mohammedbadsha805@gmail.com>
2022-06-27 18:44:47 +0600

    methodoverloading

---
+++ Methods overloading.py
@@ -0,0 +1,14 @@
+
+class mobile:
+
+    def __init__(self):
+        print("Here is mobile")
+
+class samsung(mobile):
+    def __init__(self):
+        super().__init__()
+        print("Here i am bro ")
+
+
+s = samsung()
+
--- stack_inpython.py
+++ stack_inpython.py
@@ -1,13 +1,34 @@
 
+# Python program to
+# demonstrate stack implementation
+# using list
+
+stack = []
+
+# append() function to push
+# element in the stack
+stack.append('a')
+stack.append('b')
+stack.append('c')
+stack.append("Mohammed Badsha")
+
+print('Initial stack')
+print(stack)
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+
 
 # stack in python use easely  !!
 books = []
-books.append("java")  # append() use of add somethin on satack !!
+books.append('java')  # append() use of add somethin on satack !!
 books.append("S.logic System")
 books.append("Operating System")
-books.pop()  # pop() use for delete something on stack !!
+print(books.pop())  # pop() use for delete something on stack !!
 books.pop()
 print(books)
 if not books:
     print("No Books Left !")
+
+
 
